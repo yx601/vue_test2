fd922a295ca86d8496b2d7e459e7201e267dd738
yx601 <yxyx601@gmail.com>
2018-12-03 20:26:15 +0800

    第一次

---
+++ src/components/mytest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="swiper-container"  ref="slide1">
+    <div class="swiper-wrapper" :key='slide.id' v-for="slide in slides">
+      <router-link class="swiper-slide" tag="div" :to="{name:'BookDetail',params:{id:slide.id}}">
+        <img :src="slide.img_url"/>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+export default {
+  name: 'mytest',
+  mounted () {
+    Swiper(this.$refs.slide1, {
+      pagination: this.$refs.pagination,
+      paginationClickable: true,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: 2500,
+      autoplayDisableOnInteraction: false
+    })
+  },
+  data () {
+    return {
+      slides: [
+        {id: 1, img_url: '@assets/mypics/handlebars.png'},
+        {id: 2, img_url: '@assets/mypics/pug.png'},
+        {id: 3, img_url: '@assets/mypics/stylus.png'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
